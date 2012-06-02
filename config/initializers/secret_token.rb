3378cb7769f6a993f94c4b0d028f6c5a5863d287
Ryan Duryea <aguynamedryan+oss@gmail.com>
2012-06-02 14:37:18 -0400

    initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SchoolFit::Application.config.secret_token = '57fc98992af0043f3131df6db007370eb9e7428ad24ca905f0c4987103a2cf447b6aaf6343352b376078b171f00c34bd69ec92056c86fcb6f0c9f01e9f2d0839'
