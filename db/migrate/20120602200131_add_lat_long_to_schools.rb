6c04aa15c47b1ffa4b2a9bbc0eb693d7e6503e21
Ryan Duryea <aguynamedryan+oss@gmail.com>
2012-06-03 04:54:06 -0400

    add latitude and longitude to school schema

---
+++ db/migrate/20120602200131_add_lat_long_to_schools.rb
@@ -0,0 +1,6 @@
+class AddLatLongToSchools < ActiveRecord::Migration
+  def change
+    add_column :schools, :latitude, :float
+    add_column :schools, :longitude, :float
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120602183959) do
+ActiveRecord::Schema.define(:version => 20120602200131) do
 
   create_table "schools", :force => true do |t|
     t.string   "name"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(:version => 20120602183959) do
     t.float    "fit_score"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.float    "latitude"
+    t.float    "longitude"
   end
 
 end
