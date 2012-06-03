3ed1fe094f2378639b46d51455cd961dd80f1bc0
Ryan Duryea <aguynamedryan+oss@gmail.com>
2012-06-03 04:45:00 -0400

    add School#grade

--- app/models/school.rb
+++ app/models/school.rb
@@ -1,3 +1,11 @@
 class School < ActiveRecord::Base
-  attr_accessible :address, :fit_score, :name
+  attr_accessible :latitude, :longitude, :address, :fit_score, :name
+
+  def grade
+    return 'A' if (fit_score >= 90)
+    return 'B' if (fit_score >= 80)
+    return 'C' if (fit_score >= 70)
+    return 'D' if (fit_score >= 60)
+    return 'F'
+  end
 end
--- test/unit/school_test.rb
+++ test/unit/school_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class SchoolTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def test_fit_score_to_letter_grades
+    assert_equal 'A', School.new(:fit_score => 90).grade
+    assert_equal 'B', School.new(:fit_score => 89).grade
+    assert_equal 'B', School.new(:fit_score => 80).grade
+    assert_equal 'C', School.new(:fit_score => 79).grade
+    assert_equal 'C', School.new(:fit_score => 70).grade
+    assert_equal 'D', School.new(:fit_score => 69).grade
+    assert_equal 'D', School.new(:fit_score => 60).grade
+    assert_equal 'F', School.new(:fit_score => 59).grade
+    assert_equal 'F', School.new(:fit_score => 0).grade
+  end
 end
