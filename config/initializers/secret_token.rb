1e331a747f4a13c3f0668cbf33f56fe2d88a9b07
aguynamedryan <aguynamedryan+oss@gmail.com>
2012-06-17 22:06:52 -0700

    make SECRET_TOKEN a secret

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-SchoolFit::Application.config.secret_token = '57fc98992af0043f3131df6db007370eb9e7428ad24ca905f0c4987103a2cf447b6aaf6343352b376078b171f00c34bd69ec92056c86fcb6f0c9f01e9f2d0839'
+SchoolFit::Application.config.secret_token = ENV['SECRET_TOKEN']
